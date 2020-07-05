08e083ad2a21344fafc4819d79f5e87ad67a6cd0
devacran <migueeldrg@gmail.com>
2020-07-05 15:36:00 -0500

    Se soluciono el bug de los links en el carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,98 +1,105 @@
 function script() {
-    const $menu = document.getElementById('menu')
-    const $navbarSide = document.querySelector('.navbar--side')
-    const $forwardBtn = document.getElementById('forward')
-    const $backwardBtn = document.getElementById('backward')
-    const $stripSmall = document.getElementById('carouselSmall')
-    const $stripBig = document.getElementById('carouselBig')
-    const $sections = document.querySelectorAll('section')
-    const $aboutTitle = document.getElementById('about')
-    const $projectTitle = document.getElementById('projectTitle')
-    const $projectDescription = document.getElementById('projectDescription')
-    const $carouselProjects = Array.from(
-        document.querySelectorAll('.carousel__project')
-    )
+  const $menu = document.getElementById("menu");
+  const $navbarSide = document.querySelector(".navbar--side");
+  const $forwardBtn = document.getElementById("forward");
+  const $backwardBtn = document.getElementById("backward");
+  const $stripSmall = document.getElementById("carouselSmall");
+  const $stripBig = document.getElementById("carouselBig");
+  const $sections = document.querySelectorAll("section");
+  const $aboutTitle = document.getElementById("about");
+  const $projectTitle = document.getElementById("projectTitle");
+  const $projectLink = document.getElementById("projectLink");
+  const $projectDescription = document.getElementById("projectDescription");
+  const $carouselProjects = Array.from(
+    document.querySelectorAll(".carousel__project")
+  );
 
-    const projectsInfo = (id) => {
-        const projects = {}
-        $carouselProjects.map((item, n) => {
-            let projectDesription = item.getAttribute('description')
-            let projectName = item.getAttribute('name')
-            projects[n] = { name: projectName, description: projectDesription }
-        })
-        return projects[id]
-    }
-    let moveSmall = 0
-    let moveBig = 0
-    let visibleProject = 1 //always the visible project the second
+  const projectsInfo = (id) => {
+    const projects = {};
+    $carouselProjects.map((item, n) => {
+      let projectDesription = item.getAttribute("description");
+      let projectName = item.getAttribute("name");
+      let projectLink = item.getAttribute("link");
+      projects[n] = {
+        name: projectName,
+        description: projectDesription,
+        link: projectLink,
+      };
+    });
+    return projects[id];
+  };
+  let moveSmall = 0;
+  let moveBig = 0;
+  let visibleProject = 1; //always the visible project the second
 
-    function toggleMenu() {
-        const menuWidth = parseInt($navbarSide.offsetWidth)
-        if ($menu.getAttribute('toggle') === 'false') {
-            Array.prototype.forEach.call($sections, (section) => {
-                section.style.transition = '0.5s'
-                section.style.transform = `translateX(-${menuWidth}px)`
-            })
-            if ($aboutTitle) {
-                const aboutTitleHeight = $aboutTitle.offsetHeight + 25
-                $aboutTitle.style.transition = '0.5s'
-                $aboutTitle.style.transform = `rotateZ(-90deg) translate(0px, -${aboutTitleHeight}px)`
-            }
-            $menu.setAttribute('toggle', 'true')
-            $navbarSide.style.zIndex = 0
-        } else {
-            Array.prototype.forEach.call($sections, (section) => {
-                section.style.transform = 'translateX(0)'
-            })
-            if ($aboutTitle) {
-                $aboutTitle.style.transform = `rotateZ(-90deg) translate(0px,0px)`
-                $navbarSide.style.zIndex = -1
-            }
-            $menu.setAttribute('toggle', 'false')
-        }
+  function toggleMenu() {
+    const menuWidth = parseInt($navbarSide.offsetWidth);
+    if ($menu.getAttribute("toggle") === "false") {
+      Array.prototype.forEach.call($sections, (section) => {
+        section.style.transition = "0.5s";
+        section.style.transform = `translateX(-${menuWidth}px)`;
+      });
+      if ($aboutTitle) {
+        const aboutTitleHeight = $aboutTitle.offsetHeight + 25;
+        $aboutTitle.style.transition = "0.5s";
+        $aboutTitle.style.transform = `rotateZ(-90deg) translate(0px, -${aboutTitleHeight}px)`;
+      }
+      $menu.setAttribute("toggle", "true");
+      $navbarSide.style.zIndex = 0;
+    } else {
+      Array.prototype.forEach.call($sections, (section) => {
+        section.style.transform = "translateX(0)";
+      });
+      if ($aboutTitle) {
+        $aboutTitle.style.transform = `rotateZ(-90deg) translate(0px,0px)`;
+        $navbarSide.style.zIndex = -1;
+      }
+      $menu.setAttribute("toggle", "false");
     }
-    if ($forwardBtn) {
-        $forwardBtn.addEventListener('click', () => {
-            moveCarousel('forwards')
-        })
-        $backwardBtn.addEventListener('click', () => {
-            moveCarousel('backwards')
-        })
-    }
+  }
+  if ($forwardBtn) {
+    $forwardBtn.addEventListener("click", () => {
+      moveCarousel("forwards");
+    });
+    $backwardBtn.addEventListener("click", () => {
+      moveCarousel("backwards");
+    });
+  }
 
-    function moveCarousel(direction) {
-        const smallImageWidth = parseInt($stripSmall.childNodes[1].clientWidth)
-        const bigImageWidth = parseInt($stripBig.childNodes[1].clientWidth)
-        if (direction === 'forwards') {
-            changeVisibleProjectInfo(direction)
-            moveSmall = moveSmall - smallImageWidth
-            moveBig = moveBig - bigImageWidth
-            $stripSmall.style.transform = `translateX(${moveSmall.toString()}px)`
-            $stripBig.style.transform = `translateX(${moveBig.toString()}px)`
-        } else if (direction === 'backwards') {
-            moveSmall = smallImageWidth + moveSmall
-            moveBig = bigImageWidth + moveBig
-            $stripSmall.style.transform = `translateX(${moveSmall.toString()}px)`
-            $stripBig.style.transform = `translateX(${moveBig.toString()}px)`
-            changeVisibleProjectInfo(direction)
-        }
+  function moveCarousel(direction) {
+    const smallImageWidth = parseInt($stripSmall.childNodes[1].clientWidth);
+    const bigImageWidth = parseInt($stripBig.childNodes[1].clientWidth);
+    if (direction === "forwards") {
+      changeVisibleProjectInfo(direction);
+      moveSmall = moveSmall - smallImageWidth;
+      moveBig = moveBig - bigImageWidth;
+      $stripSmall.style.transform = `translateX(${moveSmall.toString()}px)`;
+      $stripBig.style.transform = `translateX(${moveBig.toString()}px)`;
+    } else if (direction === "backwards") {
+      moveSmall = smallImageWidth + moveSmall;
+      moveBig = bigImageWidth + moveBig;
+      $stripSmall.style.transform = `translateX(${moveSmall.toString()}px)`;
+      $stripBig.style.transform = `translateX(${moveBig.toString()}px)`;
+      changeVisibleProjectInfo(direction);
     }
-    function changeVisibleProjectInfo(direction) {
-        if (direction === 'forwards') {
-            console.log(visibleProject)
-            visibleProject++
-            console.log(visibleProject)
-        } else if (direction === 'backwards') {
-            console.log('pa atras')
-            visibleProject--
-        }
-        if (visibleProject >= $carouselProjects.length || visibleProject < 0) {
-            return
-        }
-        const info = projectsInfo(visibleProject)
-        $projectTitle.innerText = info.name
-        $projectDescription.innerText = info.description
+  }
+  function changeVisibleProjectInfo(direction) {
+    if (direction === "forwards") {
+      console.log(visibleProject);
+      visibleProject++;
+      console.log(visibleProject);
+    } else if (direction === "backwards") {
+      console.log("pa atras");
+      visibleProject--;
     }
-    $menu.addEventListener('click', toggleMenu)
+    if (visibleProject >= $carouselProjects.length || visibleProject < 0) {
+      return;
+    }
+    const info = projectsInfo(visibleProject);
+    $projectTitle.innerText = info.name;
+    $projectDescription.innerText = info.description;
+    $projectLink.setAttribute("href", info.link);
+  }
+  $menu.addEventListener("click", toggleMenu);
 }
-export default script
+export default script;
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,57 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-    },
-    devServer: {
-        port: 9000,
-        open: true,
-        host: '192.168.0.105',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            // {
-            //     test: /\.css?$/,
-            //     exclude: /node_modules/,
-            //     use: ['style-loader', 'css-loader'],
-            // },
-            {
-                test: /\.styl$/,
-                loader: 'style-loader!css-loader!stylus-loader',
-            },
-            {
-                test: [/\.pug/],
-                exclude: [/node_modules/, /public/, /dist/],
-                loaders: ['html-loader', 'pug-html-loader'],
-            },
-            // {
-            //     test: /\.html?$/i,
-            //     exclude: [/node_modules/, /public/, /dist/],
-            //     use: 'html-loader',
-            // },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                exclude: [/node_modules/, /public/, /dist/],
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  devServer: {
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      // {
+      //     test: /\.css?$/,
+      //     exclude: /node_modules/,
+      //     use: ['style-loader', 'css-loader'],
+      // },
+      {
+        test: /\.styl$/,
+        loader: "style-loader!css-loader!stylus-loader",
+      },
+      {
+        test: [/\.pug/],
+        exclude: [/node_modules/, /public/, /dist/],
+        loaders: ["html-loader", "pug-html-loader"],
+      },
+      // {
+      //     test: /\.html?$/i,
+      //     exclude: [/node_modules/, /public/, /dist/],
+      //     use: 'html-loader',
+      // },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        exclude: [/node_modules/, /public/, /dist/],
+        use: [
+          {
+            loader: "file-loader",
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html'),
-            filename: path.resolve(__dirname, 'dist', 'index.html'),
-        }),
+      },
     ],
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      filename: path.resolve(__dirname, "dist", "index.html"),
+    }),
+  ],
+};
